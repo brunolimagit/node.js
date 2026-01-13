c2bedbaa59a731fc20291e37a4ce5ba06d05a458
brunolimagit <bru_jefferson@hotmail.com>
2026-01-13 00:45:11 -0300

    testando HTTP status codes

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ import express from "express";
 const server = express();
 
 //localhost:3000/curso
+//aqui estou recebendo "req" = nome | enviando "res"= <h1>curso de ${nome}</h1>
 server.get('/curso', (req, res) => {
     const nome = req.query.nome
+
+    //aqui estou usando o HTTP status codes em verificação para erros(400) bad request  = dados inválidos
+    if(!nome){
+    return res.status(400).send(`<h1>indique o nome do curso</h1>`)
+    }
     console.log('acessou a rota');
     res.send(`<h1>curso de ${nome}</h1>`);
 });
 
+//localhost:3000/api
+//aqui troquei a rota para /api e estou enviando "res"= um json em forma de obj
+server.get('/api', (req, res) =>{
+    return res.json({
+        nome: "bruno lima",
+        idade: 31,
+        nacionalidade:"brasileiro"
+    })
+})
 
 server.listen(3000);
 
